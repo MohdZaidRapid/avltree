63166cb0bc30c9810c36b02352eca0d5fcd9b69b
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-31 14:22:44 +0530

    added

---
+++ eventLoop.js
@@ -0,0 +1,16 @@
+console.log("start");
+
+setTimeout(() => {
+  console.log("hello from setTimeout");
+}, 0);
+
+async function callFromWebApi() {
+  console.log("inside async");
+  const repsonse = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await repsonse.json();
+  console.log("after  async operation");
+  console.log(data);
+}
+
+callFromWebApi();
+console.log("end");
